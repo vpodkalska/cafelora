99f9ae2a86cde2d768e4da4814b23c6fb0751381
Veronika Podkalska <vpodkalska@gmail.com>
2025-07-08 15:57:34 +0200

    uprava Header komponenty a pridani do order.jsx

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,21 +1,27 @@
 import './header.css'
 
-export const Header = ({ domu, menu, galerie, kontakt }) => {
+export const Header = ({ domu, menu, galerie, kontakt, showMenu = true }) => {
   return (
     <header>
       <div>
         <div className="header__content container">
           <div className="site-logo"></div>
+          {showMenu ? (
 
-          <div className="navigation">
-            <button id="nav-button" className="nav-btn"></button>
-            <nav className="rollout-nav nav-closed">
-              <a href="#home">{domu}</a>
-              <a href="#menu">{menu}</a>
-              <a href="#gallery">{galerie}</a>
-              <a href="#contact">{kontakt}</a>
+            <div className="navigation">
+              <button id="nav-button" className="nav-btn"></button>
+              <nav className="rollout-nav nav-closed">
+                <a href="#home">{domu}</a>
+                <a href="#menu">{menu}</a>
+                <a href="#gallery">{galerie}</a>
+                <a href="#contact">{kontakt}</a>
+              </nav>
+            </div>
+          ) : (
+            <nav className='inline-nav'>
+              <a href="/">Hlavní stránka</a>
             </nav>
-          </div>
+          ) }
         </div>
       </div>
     </header>
--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -2,20 +2,15 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 import './order.css';
+import { Header } from '../components/Header/header';
+import { Footer } from '../components/Footer/footer';
+
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <div className="page">
-      <header>
-        <div className="container header__content">
-          <div className="site-logo"></div>
-
-          <nav className="inline-nav">
-            <a href="/">Hlavní stránka</a>
-          </nav>
-
-        </div>
-      </header>
+      <Header 
+      showMenu = {false} />
 
       <main className="order">
         <div className="container order__content">
@@ -44,14 +39,8 @@ document.querySelector('#root').innerHTML = render(
           </div>
         </div>
       </main>
+      <Footer />
 
-      <footer>
-        <div className="container">
-          <div className="footer__content">
-            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-          </div>
-        </div>
-      </footer>
     </div>
   </div>
 );
